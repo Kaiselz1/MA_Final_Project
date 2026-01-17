f1801ca36d6ce215bedc888b50a6ed6e6aa4ecf0
Nhngobhz <realbunthai@gmail.com>
2026-01-17 22:15:49 +0700

    added some api files

---
+++ lib/api/api_end_point.dart
@@ -0,0 +1,6 @@
+class ApiEndPoint {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String validateToken = '/validate-token';
+  static const String logout = '/logout';
+}
